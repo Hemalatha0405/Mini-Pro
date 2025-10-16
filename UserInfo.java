9a240cf830a5a26c0a0d9ce5726067e30892d503
Hemalatha0405 <146410424+Hemalatha0405@users.noreply.github.com>
2025-10-16 17:37:24 +0530

    Add files via upload

---
+++ UserInfo.java
@@ -0,0 +1,13 @@
+package com.example.Info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInfo.class, args);
+	}
+
+}
